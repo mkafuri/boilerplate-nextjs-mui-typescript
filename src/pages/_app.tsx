1321f025765ab23aa0330c2444c8fff5e41d0798
mkafuri <mkafuri@g4educacao.com.br>
2022-06-28 09:09:30 -0300

    improvements

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,31 +2,31 @@ import React from 'react'
 import Head from 'next/head'
 import type { AppProps } from 'next/app'
 import { CssBaseline, ThemeProvider } from '@mui/material'
-import createEmotionCache from '../utility/createEmotionCache'
+import createEmotionCache from '../utils/createEmotionCache'
 import { CacheProvider, EmotionCache } from '@emotion/react'
-import theme from '../styles/theme'
+import { lightTheme } from '../styles/theme'
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache()
 
 interface MyAppProps extends AppProps {
-	emotionCache?: EmotionCache
+  emotionCache?: EmotionCache
 }
 
 const MyApp = (props: MyAppProps) => {
-	const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
-	return (
-		<CacheProvider value={emotionCache}>
-			<Head>
-				<meta name="viewport" content="initial-scale=1, width=device-width" />
-			</Head>
-			<ThemeProvider theme={theme}>
-				{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-				<CssBaseline />
-				<Component {...pageProps} />
-			</ThemeProvider>
-		</CacheProvider>
-	)
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={lightTheme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
 
 export default MyApp
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,18 +1,80 @@
 import { red } from '@mui/material/colors'
-import { createTheme } from '@mui/material/styles'
+import { createTheme, responsiveFontSizes } from '@mui/material/styles'
 
-const theme = createTheme({
-	palette: {
-		primary: {
-			main: '#02182B',
-		},
-		secondary: {
-			main: '#ff6666',
-		},
-		error: {
-			main: red.A400,
-		},
-	},
+let lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#FA7268',
+      light: '#FF958C',
+      dark: '#FF6559',
+      contrastText: '#FFFFFF',
+    },
+    secondary: {
+      light: '#757ce8',
+      main: '#5E5CE5',
+      dark: '#6461FF',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#EBF1F7',
+      paper: '#FFFFFF',
+    },
+    text: {
+      primary: '#475059',
+      secondary: '#7A8999',
+    },
+    info: {
+      main: '#182D3D',
+    },
+  },
+  shape: {
+    borderRadius: 8,
+  },
+  typography: {
+    fontFamily: 'Montserrat'
+  }
+})
+lightTheme = responsiveFontSizes(lightTheme)
+
+let darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#FF6559',
+      light: '#FF958C',
+      dark: '#FA7268',
+      contrastText: '#FFFFFF',
+    },
+    secondary: {
+      light: '#757ce8',
+      main: '#5E5CE5',
+      dark: '#6461FF',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#182D3D',
+      paper: '#476382',
+    },
+    text: {
+      primary: '#EBF1F7',
+      secondary: '#FCFEFF',
+    },
+    info: {
+      main: '#5E5CE5',
+    },
+  },
+  shape: {
+    borderRadius: 8,
+  },
+  typography: {
+    fontFamily: 'Montserrat'
+  }
 })
 
-export default theme
+darkTheme = responsiveFontSizes(darkTheme)
+export { lightTheme, darkTheme }
